14134aeb9e8b6b13a061b202f13c46bb1b8e8f4b
Elena Sacristán Sacristán <44655750+elenasacristan@users.noreply.github.com>
2020-02-06 23:15:21 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,45 @@
+import os
+import env
+import json
+from flask import Flask, render_template, request, flash
+
+app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
+
+@app.route('/')
+def index():
+    return render_template("home.html", page_title="Home")
+
+@app.route('/about')
+def about():
+    data = []
+    with open('data/company.json','r') as json_data:
+        data = json.load(json_data)
+
+    return render_template("about.html", page_title="About", company=data)
+
+@app.route('/about/<name_member>')
+def about_member(name_member):
+    member = {}
+    with open('data/company.json','r') as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"] == name_member:
+                member = obj
+
+    return render_template("member.html", member=member)
+
+@app.route('/contact', methods=["GET", "POST"])
+def contact():
+    if request.method == "POST":
+        flash("Thanks {}, we have received your request.".format(request.form["name"]))
+    return render_template("contact.html", page_title="Contact us")
+
+@app.route('/careers')
+def careers():
+    return render_template("careers.html", page_title="Careers")
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get("IP"), 
+            port=int(os.environ.get("PORT")), 
+            debug=True)
